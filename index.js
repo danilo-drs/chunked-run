1ab80b30048fca7fcf64aebb5cd7a39b277148ac
Danilo Ribeiro <danilo.ribeiro.silva@accenture.com>
2020-03-06 17:08:35 -0300

    add chunked-run

---
+++ index.js
@@ -0,0 +1,27 @@
+import queue from 'async/queue';
+
+const chunkedRun = (dataArray, processFunction, chunkSize) => new Promise((resolve) => {
+  if (dataArray.length === 0) resolve([]);
+
+  const finalResult = [];
+
+  const queueEngine = queue(async (item) => {
+    const result = await processFunction(item.item);
+
+    finalResult.push({ ...item, result });
+  }, chunkSize);
+
+  queueEngine.drain(() => {
+    resolve(finalResult
+      .sort((act, next) => (act.index > next.index ? 1 : -1))
+      .map(item => item.result || item.error));
+  });
+
+  queueEngine.error((error, item) => {
+    finalResult.push({ ...item, error });
+  });
+
+  queueEngine.push(dataArray.map((item, index) => ({ index, item })));
+});
+
+export default chunkedRun;
